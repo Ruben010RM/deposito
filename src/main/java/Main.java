0ecd3457bf93801fa3caf135f1da9b5c8b6bd139
Ruben <rubenrm.contact@gmail.com>
2025-03-14 21:46:24 +0100

    Commit final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,23 @@
 import cuenta.CCuenta;
-
+/**
+ * @author Ruben Rodriguez Martinez
+ * */
 public class Main {
-
+    /**
+    * Método principal que crea una cuenta bancaria y muestra su saldo.
+    */
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es "+ saldoActual );
     }
+      /**
+     * Realiza operaciones de retiro e ingreso en una cuenta bancaria.
+     * @param cuenta1 La cuenta bancaria en la que se realizarán las operaciones.
+     */
     public void operativa_cuenta(CCuenta cuenta1){
         try {
             cuenta1.retirar(2300);
--- src/main/java/cuenta/CCuenta.java
+++ src/main/java/cuenta/CCuenta.java
@@ -1,36 +1,65 @@
 package cuenta;
-
+/**
+ * Clase que representa una cuenta bancaria.
+ * @author Ruben Rodriguez Martinez
+ */
 public class CCuenta {
 
-
+    /** Nombre del titular de la cuenta */
     private String nombre;
+    /** Número de cuenta */
     private String cuenta;
+    /** Saldo actual de la cuenta */
     private double saldo;
+    /** Tipo de interés de la cuenta */
     private double tipoInterés;
-
+    
+    /**
+     * Constructor por defecto.
+     */
     public CCuenta()
     {
     }
-
+    /**
+     * Constructor con parámetros.
+     * @param nom Nombre del titular de la cuenta
+     * @param cue Número de cuenta
+     * @param sal Saldo inicial
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * @return El saldo actual
+     */
     public double estado()
     {
         return saldo;
     }
-
+    
+    /**
+     * Ingresa una cantidad en la cuenta.
+     * @param cantidad Cantidad a ingresar
+     * @throws Exception Si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    
+    /**
+     * Retira una cantidad de la cuenta.
+     * @param cantidad Cantidad a retirar
+     * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
